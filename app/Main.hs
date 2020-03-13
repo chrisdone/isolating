4f5d39fd45075686575b6e1705812aa79b565db4
Chris Done <chrisdone@gmail.com>
2020-03-13 13:21:10 +0000

    Comp

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -17,6 +20,7 @@ import qualified Data.Map.Strict as M
 import           Data.Pool
 import           Data.Text (Text)
 import           Data.Time
+import           Data.Validation
 import           Database.Persist.Postgresql
 import qualified Forge.Generate as Forge
 import qualified Forge.Internal.Types as Forge
@@ -74,80 +78,96 @@ postCreateGroupR = do
         (M.fromListWith
            (<>)
            (map (first Forge.Key . second (pure . Forge.TextInput)) inputs))
-  case Forge.generate inputMap (Forge.verified createGroupForm) of
-    _ -> undefined
+  case runIdentity (Forge.generate inputMap (Forge.verified createGroupForm)) of
+    Forge.Generated {generatedView = html, generatedValue = v} ->
+      case v of
+        Failure _errs ->
+          htmlWithUrl
+            (layoutWrapper (div_ [class_ "wrap"] (startGroupHtml html)))
+        Success postCode -> pure "OK, making!"
 
 getHomeR :: Handler (Html ())
 getHomeR = do
   htmlWithUrl
-    (do doctype_
-        url <- ask
-        html_
-          (do head_
-                (do link_ [rel_ "shortcut icon", href_ "#"]
-                    title_ "Self Isolating (COVID-19)"
-                    link_
-                      [rel_ "stylesheet", type_ "text/css", href_ (url AppCssR)])
-              body_
+    (layoutWrapper
+       (do div_
+             [class_ "wrap"]
+             (div_
+                [class_ "home-choice"]
                 (do div_
-                      [class_ "heading"]
-                      (div_
-                         [class_ "wrap"]
-                         (h1_ "Self Isolating Coronavirus (COVID-19)"))
+                      [class_ "choice"]
+                      (do h2_ "I have a code"
+                          p_
+                            "If someone has created a group for your street or apartment block, \
+                                         \you should have received a code in your letterbox or in person."
+                          url <- ask
+                          form_
+                            [action_ (url HomeR), method_ "POST"]
+                            (do p_
+                                  (do label_ "Postcode: "
+                                      input_ [type_ "text", name_ "postcode"])
+                                p_
+                                  (do label_ "Member Code: "
+                                      input_ [type_ "text", name_ "code"])
+                                p_ (button_ "LOGIN")))
                     div_
-                      [class_ "wrap"]
-                      (div_
-                         [class_ "home-choice"]
-                         (do div_
-                               [class_ "choice"]
-                               (do h2_ "I have a code"
-                                   p_
-                                     "If someone has created a group for your street or apartment block, \
-                                      \you should have received a code in your letterbox or in person."
-                                   form_
-                                     [action_ (url HomeR), method_ "POST"]
-                                     (do p_
-                                           (do label_ "Postcode: "
-                                               input_
-                                                 [ type_ "text"
-                                                 , name_ "postcode"
-                                                 ])
-                                         p_
-                                           (do label_ "Member Code: "
-                                               input_
-                                                 [type_ "text", name_ "code"])
-                                         p_ (button_ "LOGIN")))
-                             div_
-                               [class_ "choice"]
-                               (do h2_
-                                     "Start a group for your street or block of apartments"
-                                   p_
-                                     "Before starting a group, please confirm that there is not \
-                                      \already a group created for your street or apartment block."
-                                   form_
-                                     [action_ (url HomeR), method_ "POST"]
-                                     (do relaxHtmlT (Forge.view (Forge.verified createGroupForm))
-                                         p_ (button_ "GET CODE")))))
-                    div_
-                      [class_ "wrap"]
-                      (do h3_ "Information"
-                          p_
-                            (do "UK Government information: "
-                                a_
-                                  [ href_
-                                      "https://www.gov.uk/government/topical-events/coronavirus-covid-19-uk-government-response"
-                                  ]
-                                  "Coronavirus (COVID-19): what you need to do")
-                          p_
-                            "This is a voluntarily run service to aid small communities (streets and tower blocks) to self-report, coordinate, and ask for help, during the self-isolation stage of this pandemic."
-                          p_
-                            (strong_
-                               "Isolating.org is NOT a government-run web site.")
-                          h3_ "Why are codes needed?"
-                          p_
-                            "To provide privacy for communities. You can only get a code if a group creator \
-                             \as provided you with one, via your letterbox or handed to you in person. This \
-                            \protects against abuse."))))
+                      [class_ "choice"]
+                      (startGroupHtml
+                         (Forge.view (Forge.verified createGroupForm)))))
+           div_
+             [class_ "wrap"]
+             (do h3_ "Information"
+                 p_
+                   (do "UK Government information: "
+                       a_
+                         [ href_
+                             "https://www.gov.uk/government/topical-events/coronavirus-covid-19-uk-government-response"
+                         ]
+                         "Coronavirus (COVID-19): what you need to do")
+                 p_
+                   "This is a voluntarily run service to aid small communities (streets and tower blocks) to self-report, coordinate, and ask for help, during the self-isolation stage of this pandemic."
+                 p_ (strong_ "Isolating.org is NOT a government-run web site.")
+                 h3_ "Why are codes needed?"
+                 p_
+                   "To provide privacy for communities. You can only get a code if a group creator \
+                                \as provided you with one, via your letterbox or handed to you in person. This \
+                               \protects against abuse.")))
+
+--------------------------------------------------------------------------------
+-- Page wrapper
+
+layoutWrapper ::
+  MonadReader (Route App -> Text) m => HtmlT m b -> HtmlT m b
+layoutWrapper inner = do
+  doctype_
+  url <- ask
+  html_
+    (do head_
+          (do link_ [rel_ "shortcut icon", href_ "#"]
+              title_ "Self Isolating Coronavirus (COVID-19)"
+              link_ [rel_ "stylesheet", type_ "text/css", href_ (url AppCssR)])
+        body_
+          (do div_
+                [class_ "heading"]
+                (div_
+                   [class_ "wrap"]
+                   (h1_ "Self Isolating Coronavirus (COVID-19)"))
+              inner))
+
+--------------------------------------------------------------------------------
+-- Start a group
+
+startGroupHtml :: HtmlT Identity () -> Lucid App ()
+startGroupHtml formHtml = do
+  h2_ "Start a group for your street or block of apartments"
+  p_
+    "Before starting a group, please confirm that there is not \
+                       \already a group created for your street or apartment block."
+  url <- ask
+  form_
+    [action_ (url HomeR), method_ "POST"]
+    (do relaxHtmlT formHtml
+        p_ (button_ "GET CODE"))
 
 --------------------------------------------------------------------------------
 -- Forms
